30b2e61d50a6a87c6d6e09e1af73c41bc89a8034
Qazi Mudassar Tanveer Ahmad <34540384+mudassarahmad09@users.noreply.github.com>
2023-12-15 19:27:22 +0500

    Refactor the result type in engine and refactor the all app where is needed after breaking the test

--- QuizEngine/Resulte.swift
+++ QuizEngine/Resulte.swift
@@ -7,20 +7,8 @@
 
 import Foundation
 
-public struct Resulte<Question: Hashable, Answer>: Hashable {
+public struct Resulte<Question: Hashable, Answer> {
     public var answer: [Question: Answer]
     public var score: Int
     
-    public init(answer: [Question : Answer], score: Int) {
-        self.answer = answer
-        self.score = score
-    }
-    
-    public func hash(into hasher: inout Hasher) {
-        hasher.combine(score)
-    }
-    
-    public static func == (lhs: Resulte<Question, Answer>, rhs: Resulte<Question, Answer>) -> Bool {
-        return lhs.score == rhs.score
-    }
 }
