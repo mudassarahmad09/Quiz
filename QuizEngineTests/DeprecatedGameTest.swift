189f8658b8a8f2e76c25b67bccab8aa9736a943e
Qazi Mudassar Tanveer Ahmad <34540384+mudassarahmad09@users.noreply.github.com>
2023-12-20 18:17:35 +0500

    add some more code into the depreacted file so we can delete the file at the end

--- QuizEngineTests/DeprecatedGameTest.swift
+++ QuizEngineTests/DeprecatedGameTest.swift
@@ -11,8 +11,8 @@ import QuizEngine
 @available(*, deprecated)
 class DeprecatedGameTest: XCTestCase {
     
-    let router = RouterSpy()
-    var game: Game<String, String, RouterSpy>!
+    private let router = RouterSpy()
+    private var game: Game<String, String, RouterSpy>!
     
     override func setUp() {
         super.setUp()
@@ -37,5 +37,18 @@ class DeprecatedGameTest: XCTestCase {
         router.answerCallback("A1")
         router.answerCallback("A2")
         XCTAssertEqual(router.routedResult!.score, 2)
+    }
+    
+    private class RouterSpy: Router {
+        var routedResult: Resulte<String, String>? = nil
+        var answerCallback: (String) -> Void = {_ in}
+        
+        func routeTo(question: String, answerCallback: @escaping (String) -> Void) {
+            self.answerCallback = answerCallback
+        }
+        
+        func routeTo(result: Resulte<String, String>) {
+            routedResult = result
+        }
     }
 }
