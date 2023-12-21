f181b59fa81f0e6e982ecc78484a476f4d50550c
Qazi Mudassar Tanveer Ahmad <34540384+mudassarahmad09@users.noreply.github.com>
2023-12-21 13:46:35 +0500

    Change variable name

--- QuizEngineTests/QuizTest.swift
+++ QuizEngineTests/QuizTest.swift
@@ -10,32 +10,32 @@ import QuizEngine
 
 class QuizTestTest: XCTestCase {
     
-    private let router = RouterSpy()
+    private let delegate = RouterSpy()
     private var game: Game<String, String, RouterSpy>!
     
     override func setUp() {
         super.setUp()
-        game = startGame(questions: ["Q1", "Q2"], router: router, correctAnswer: ["Q1": "A1", "Q2": "A2"])
+        game = startGame(questions: ["Q1", "Q2"], router: delegate, correctAnswer: ["Q1": "A1", "Q2": "A2"])
     }
     
     func test_startGame_answerZeroOutTwoCorrectly_scoresZero() {
-        router.answerCallback("worng")
-        router.answerCallback("worng")
+        delegate.answerCallback("worng")
+        delegate.answerCallback("worng")
         
-        XCTAssertEqual(router.routedResult!.score, 0)
+        XCTAssertEqual(delegate.routedResult!.score, 0)
     }
     
     func test_startGame_answerOneOutTwoCorrectly_scoresOne() {
-        router.answerCallback("A1")
-        router.answerCallback("worng")
+        delegate.answerCallback("A1")
+        delegate.answerCallback("worng")
         
-        XCTAssertEqual(router.routedResult!.score, 1)
+        XCTAssertEqual(delegate.routedResult!.score, 1)
     }
     
     func test_startGame_answerTwoOutTwoCorrectly_scoresTwo() {
-        router.answerCallback("A1")
-        router.answerCallback("A2")
-        XCTAssertEqual(router.routedResult!.score, 2)
+        delegate.answerCallback("A1")
+        delegate.answerCallback("A2")
+        XCTAssertEqual(delegate.routedResult!.score, 2)
     }
     
     private class RouterSpy: Router {
