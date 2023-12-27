980776a22ef02d4c7c58b7dde86aff4d76473c11
Qazi Mudassar Tanveer Ahmad <34540384+mudassarahmad09@users.noreply.github.com>
2023-12-27 11:38:36 +0500

    test_noAnswer_scoreZero test added

--- QuizEngineTests/ScoreTest.swift
+++ QuizEngineTests/ScoreTest.swift
@@ -5,4 +5,16 @@
 //  Created by Qazi Mudassar on 27/12/2023.
 //
 
-import Foundation
+import XCTest
+
+class ScoreTest: XCTestCase {
+    func test_noAnswer_scoreZero() {
+        XCTAssertEqual(BasicScore.score(for: []), 0)
+    }
+    
+    private class BasicScore {
+       static func score(for: [Any]) -> Int {
+           0
+       }
+    }
+}
