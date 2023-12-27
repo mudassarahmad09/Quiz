bc2e263c902abc2b82a11b617b2aa578c5535860
Qazi Mudassar Tanveer Ahmad <34540384+mudassarahmad09@users.noreply.github.com>
2023-12-27 11:42:43 +0500

    test_oneWorngAnswer_scoreZero test added using TDD

--- QuizEngineTests/ScoreTest.swift
+++ QuizEngineTests/ScoreTest.swift
@@ -9,11 +9,15 @@ import XCTest
 
 class ScoreTest: XCTestCase {
     func test_noAnswer_scoreZero() {
-        XCTAssertEqual(BasicScore.score(for: []), 0)
+        XCTAssertEqual(BasicScore.score(for: [], comparingTo: []), 0)
+    }
+    
+    func test_oneWorngAnswer_scoreZero() {
+        XCTAssertEqual(BasicScore.score(for: ["worng"], comparingTo: ["correct"]), 0)
     }
     
     private class BasicScore {
-       static func score(for: [Any]) -> Int {
+        static func score(for: [Any], comparingTo: [Any]) -> Int {
            0
        }
     }
